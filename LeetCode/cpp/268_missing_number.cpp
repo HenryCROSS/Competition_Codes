5bdfaecd673ced414ae8e2e76a31582c5cf7a4e1
henrycross <henry583050973@gmail.com>
2022-05-28 14:04:10 -0400

    final fix

--- LeetCode/cpp/268_missing_number.cpp
+++ LeetCode/cpp/268_missing_number.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,12 +8,14 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int sz = nums.size();
+        
+        sort(nums.begin(), nums.end());
 
-        for (int idx = 0; idx <= sz; ++idx) {
+        for (int idx = 0; idx < sz; ++idx) {
             if(nums.at(idx) != idx)
                 return idx;
         }
 
-        return 0;
+        return nums.size();
     }
 };
