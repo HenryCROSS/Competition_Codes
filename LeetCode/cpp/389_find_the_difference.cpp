ef636f400f425093aac8c1895ce435d6865961f0
henrycross <henry583050973@gmail.com>
2022-05-28 14:43:15 -0400

    fixed

--- LeetCode/cpp/389_find_the_difference.cpp
+++ LeetCode/cpp/389_find_the_difference.cpp
@@ -5,6 +5,9 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int len = s.size();
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
         for (int idx = 0; idx < len; ++idx) {
             if (s.at(idx) != t.at(idx))
                 return t.at(idx);
