9e27c749afb68b3a5efd22af8105c1a3c1bc2abf
henrycross <henry583050973@gmail.com>
2022-05-29 15:07:24 -0400

    try

--- LeetCode/cpp/13_roman_to_integer.cpp
+++ LeetCode/cpp/13_roman_to_integer.cpp
@@ -2,57 +2,53 @@
 using namespace std;
 
 class Solution {
-public:
-    int romanToInt(string s) {
-        const char* SYMBOLS = "MDCLXVI";
-        const char* pivot = SYMBOLS;
-        int len = s.size();
-        int sum = 0;
+    public:
+        int romanToInt(string s) {
+            // const char* SYMBOLS = "MDCLXVI";
+            const char* SYMBOLS = "IVXLCDM";
+            const char* pivot = SYMBOLS;
+            int len = s.size();
+            int tmp = 0;
+            int ans = 0;
 
-        for (int idx = 0; idx < len; ++idx) {
-            if(s[idx] != *pivot)
-            {
-                switch (s[idx]) {
-                    case 'M':
-                        sum -= 1000;
-                    case 'D':
-                        sum -= 500;
-                    case 'C':
-                        sum -= 100;
-                    case 'L':
-                        sum -= 50;
-                    case 'X':
-                        sum -= 10;
-                    case 'V':
-                        sum -= 5;
-                    case 'I':
-                        sum -= 1;
-                }
-            }
-            else
-            {
-                switch (s[idx]) {
-                    case 'M':
-                        sum += 1000;
-                    case 'D':
-                        sum += 500;
-                    case 'C':
-                        sum += 100;
-                    case 'L':
-                        sum += 50;
-                    case 'X':
-                        sum += 10;
-                    case 'V':
-                        sum += 5;
-                    case 'I':
-                        sum += 1;
+
+            for (int idx = len - 1; idx >= 0; --idx) {
+                if(*pivot == 'I' && s[idx] == 'I')
+                    ++ans;
+                else
+                {
+                    if(*pivot == s[idx])
+                    {
+                        tmp *= -1;
+                        ++pivot;
+                    }
+
+                    switch (s[idx]) {
+                        case 'M':
+                            tmp += 1000;
+                        case 'D':
+                            tmp += 500;
+                        case 'C':
+                            tmp += 100;
+                        case 'L':
+                            tmp += 50;
+                        case 'X':
+                            tmp += 10;
+                        case 'V':
+                            tmp += 5;
+                        case 'I':
+                            tmp += 1;
+                    }
+
+                    if(*pivot == s[idx])
+                    {
+                        ans += tmp;
+                        tmp = 0;
+                    }
                 }
 
-                if(s[idx] != 'I')
-                    pivot++;
             }
+
+            return ans;
         }
-
-        return sum;
-    }
 };
